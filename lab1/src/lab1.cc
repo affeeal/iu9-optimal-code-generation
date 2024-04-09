f00f75c8cf05252c18122ce4f68989ec21f75662
Ilya Afanasyev <affeeal@yandex.ru>
2024-04-09 21:35:38 +0300

    finish plugin init

--- lab1/src/lab1.cc
+++ lab1/src/lab1.cc
@@ -13,21 +13,82 @@
  *
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <https://www.gnu.org/licenses/>.
-*/
+ */
 
 #include <iostream>
+#include <memory>
 
-#include "gcc-plugin.h"  // must be the first gcc header to include
+// clang-format off
+#include "gcc-plugin.h"  // the first gcc header to be included
+//clang-format on
+
+#include "context.h"
 #include "plugin-version.h"
+#include "tree-pass.h"
 
 int plugin_is_GPL_compatible;  // asserts the plugin is licensed under the
                                // GPL-compatible license
 
+namespace {
+
+constexpr auto kPrintPassData = pass_data{
+  .type = GIMPLE_PASS,
+  .name = "print",
+  .optinfo_flags = OPTGROUP_NONE,
+  .tv_id = TV_NONE,
+  .properties_required = PROP_gimple_any,
+  .properties_provided = 0,
+  .properties_destroyed = 0,
+  .todo_flags_start = 0,
+  .todo_flags_finish = 0,
+};
+
+class PrintPass final : public gimple_opt_pass {
+ public:
+  PrintPass(gcc::context* ctxt) : gimple_opt_pass(kPrintPassData, ctxt) {}
+
+  PrintPass* clone() override;
+  unsigned int execute(function* fun) override;
+};
+
+PrintPass* PrintPass::clone() {
+  return this;
+}
+
+unsigned int PrintPass::execute(function* fun) {
+  // TODO
+  return 0;
+}
+
+const auto kPrintPass = std::make_unique<PrintPass>(g);
+
+}  // namespace
+
 int plugin_init(struct plugin_name_args *plugin_info,
                 struct plugin_gcc_version *version) {
   if (!plugin_default_version_check(version, &gcc_version)) {
+    std::cerr << "This GCC plugin is for version " << GCCPLUGIN_VERSION_MAJOR
+              << "." << GCCPLUGIN_VERSION_MINOR << "\n";
     return 1;
   }
+
+  auto plugin_additonal_info = ::plugin_info{
+      .version = "1.0",
+      .help = "GCC GIMPLE/IR print plugin",
+  };
+
+  register_callback(plugin_info->base_name, PLUGIN_INFO, nullptr,
+                    &plugin_additonal_info);
+
+  auto pass_info = register_pass_info{
+      .pass = kPrintPass.get(),
+      .reference_pass_name = "ssa",
+      .ref_pass_instance_number = 1,
+      .pos_op = PASS_POS_INSERT_AFTER,
+  };
+
+  register_callback(plugin_info->base_name, PLUGIN_PASS_MANAGER_SETUP, nullptr,
+                    &pass_info);
 
   return 0;
 }
--- lab1/tests/test.cc
+++ lab1/tests/test.cc
@@ -1,5 +1,14 @@
-#include <iostream>
+void Bar() {}
+
+int Foo(const int x, const int x_doubled) {
+  return 3 * x + x_doubled;
+}
 
 int main() {
-  std::cout << "Test\n";
+  Bar();
+
+  auto x = 0;
+  auto y = Foo(x, x * 2);
+
+  return y % 2;
 }
